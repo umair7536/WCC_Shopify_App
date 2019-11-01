ede977c458b5063b053fe9592120f96347c18745
Mustafa Liaqat <mustafa@developify.net>
2019-11-01 12:18:01 +0500

    Shopify Install app issue is fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 //Route::get('/', function () { return redirect('/admin/home'); });
-Route::get('/', 'Admin\ShopifyController@verify')->name('shopify.verify_install');
+Route::get('/', 'Admin\ShopifyController@verifyShopify')->name('shopify.verify_install');
 
 //use Route for register:R
 Auth::routes();
