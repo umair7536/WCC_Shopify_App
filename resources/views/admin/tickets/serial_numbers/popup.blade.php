7ff4f350c02cdb36e639de3d01b56cc3db39c5d4
Mustafa Liaqat <mustafa@developify.net>
2019-11-05 13:38:00 +0500

    Scroll issue is fixed

--- resources/views/admin/tickets/serial_numbers/popup.blade.php
+++ resources/views/admin/tickets/serial_numbers/popup.blade.php
@@ -2,7 +2,7 @@
     <button type="button" id="closeBtn" class="close" data-dismiss="modal" aria-hidden="true"></button>
     <h4 class="modal-title">Other @lang('global.tickets.fields.ticket_plural') of <b class="font-blue-madison">{{ $serial_number }}</b></h4>
 </div>
-<div class="modal-body">
+<div class="modal-body" style="max-height: 400px; overflow-y: auto;">
     <div class="form-body">
 
         <div class="table-container">
@@ -38,10 +38,10 @@
                                 <th>Comments</th>
                                 <td>{{ $single_ticket->technician_remarks }}</td>
                             </tr>
+                            <tr>
+                                <td colspan="2" style="text-align: center;"></td>
+                            </tr>
                         @endforeach
-                        <tr>
-                            <td colspan="2">&nbsp;</td>
-                        </tr>
                     @else
                         <tr>
                             <td>Other @lang('global.tickets.fields.ticket_plural') not found.</td>
