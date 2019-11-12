67e653edfec459a55aa23c0e270a1e8c41a5c7f6
Mustafa Liaqat <mustafa@developify.net>
2019-11-12 18:53:54 +0500

    htts foce is added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,9 +14,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        }
 
     }
 
